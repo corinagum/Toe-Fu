fb3406fbf3634d4d862347677177d316a2ead429
Corina <corina.aj@gmail.com>
2016-01-23 12:23:29 -1000

    add Player class and spritesheet

--- public/js/states/game.js
+++ public/js/states/game.js
@@ -4,10 +4,13 @@
 // class constructor
 ToeFu.Game = function() {
 
+  this.player_1;
+
 };
 
 ToeFu.Game.prototype.create = function (){
-
+  this.player_1 = new ToeFu.Player( this.game, 0 );
+  this.game.add.existing(this.player_1);
 };
 
 ToeFu.Game.prototype.update = function() {
