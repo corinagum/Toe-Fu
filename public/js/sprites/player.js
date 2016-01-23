a700ca71b6ed7ef401150a0fdd698f6551b90d4e
Corina <corina.aj@gmail.com>
2016-01-23 12:39:40 -1000

    add idle animation to player

--- public/js/sprites/player.js
+++ public/js/sprites/player.js
@@ -1,17 +1,37 @@
-// sprite class constructor
+(function() {
+  // private static variable
 
-ToeFu.Player = function(game, id, name) {
-  this.game = game;
-  this.id = id;
-  this.name = name ? name : 'Player '+(id+1);
+  var ANIMATIONS = {
+    IDLE : {
+      name : 'idle',
+      frames : [0,1,2,3],
+      fps : 5
+    }
+  };
 
-  // super constructor call
-  Phaser.Sprite.call(this, game, 0, 0, ToeFu.ASSETS.SPRITESHEET.PLAYER.name);
-};
+  // sprite class constructor
 
-// extend Sprite prototype
-ToeFu.Player.prototype = Object.create(Phaser.Sprite.prototype, {
-  constructor: {
-    value: ToeFu.Player
-  }
-});
+  ToeFu.Player = function(game, id, name) {
+    this.game = game;
+    this.id = id;
+    this.name = name ? name : 'Player '+(id+1);
+
+    // super constructor call
+    Phaser.Sprite.call(this, game, 0, 0, ToeFu.ASSETS.SPRITESHEET.PLAYER.name);
+
+    // set animations
+    this.animations.add(ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.frames);
+
+    // play the initial animation
+    this.animations.play(ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.fps, true); // true is for looping
+
+  };
+
+  // extend Sprite prototype
+  ToeFu.Player.prototype = Object.create(Phaser.Sprite.prototype, {
+    constructor: {
+      value: ToeFu.Player
+    }
+  });
+
+})();
