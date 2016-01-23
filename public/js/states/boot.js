91d83148e6554a64035e570672a6da28a1702844
Corina <corina.aj@gmail.com>
2016-01-23 11:56:07 -1000

    add asset auto loader and a single asset for player

--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -8,7 +8,19 @@ ToeFu.Boot = function() {
 
 ToeFu.Boot.prototype.preload = function() {
   // preload all assets
-};
+  //autoload each asset by type
+    Object.keys(ToeFu.ASSETS).forEach(function(type){
+    for( var asset in ToeFu.ASSETS[type] ){
+      ToeFu.game.load[ type.toLowerCase() ](
+        ToeFu.ASSETS[type][ asset ].name,
+        ToeFu.ASSETS[type][ asset ].path,
+        ToeFu.ASSETS[type][ asset ].width,
+        ToeFu.ASSETS[type][ asset ].height,
+        ToeFu.ASSETS[type][ asset ].frames
+      );
+    }
+  });
+}; // close line 9: ToeFu.Boot.prototype.preload = function etc.
 
 ToeFu.Boot.prototype.create = function() {
   // switch to game state
